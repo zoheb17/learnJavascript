241eb2823171f732929517de52fc8d0a9d7cbca4
zoheb17 <sayyedzohebuddin61859@gmail.com>
2025-10-26 13:57:23 +0000

    learning js

---
+++ jsFunction/functionExpressions.js
@@ -0,0 +1,30 @@
+// function in Js
+
+
+function addNumber(z1 , z2){
+    let result= z1 * z2
+    console.log(result);
+}
+addNumber( 5, 7)
+
+
+// for more easy explantion
+
+// JavaScript Function and Function Expressions
+
+// A function is an independent block of code that performs a specific task, while a function expression is a way to store functions in variables.
+
+
+
+// Example 1: JavaScript Function Call
+
+// If we run the above code, we won't get any output. But why?
+
+// It's because creating a function doesn't mean we are executing the code inside it. In other words, the function is ready and available for us to execute whenever we choose.
+
+// And if we want to use the function, we need to call it.
+
+function greet(name){
+    console.log(`hello ${name}`);
+}
+ greet(ibrahim)
